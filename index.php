7ac46640bd1e4533f82e71e6e50fa7d80124fad1
Arielesales <78573010+Arielesales@users.noreply.github.com>
2021-02-04 21:41:36 -0300

    iniciar primeira versão do repositorio index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+ <!DOCTYPE html>
+    <html lang="pt-br">
+      <head>
+        <title>Repositório de PSI</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1>
+          <?php   
+            echo "Esse é o meu primeiro repositório!";
+          ?>
+        </h1>
+      </body>
+    </html>
